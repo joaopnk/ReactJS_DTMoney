d7dca37c343a60ffa5b9d5e42ba1c5f1aaaf9cae
João <joaopedronk22@gmail.com>
2022-10-20 14:39:09 -0300

    Tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions(){
     return (
         <div>
             <Header />
             <Summary />
+
+            <TransactionsContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    R$ 12.000,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/09/2022</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Jantar</td>
+                            <td>
+                                <PriceHighlight variant="outcome">
+                                - R$ 29,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Lazer</td>
+                            <td>20/09/2022</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
     )
 }
