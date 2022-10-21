144f63243b0852485d81097e72f163f7d3b77174
João <joaopedronk22@gmail.com>
2022-10-21 14:32:31 -0300

    Realizando requisição HTTP
    Utilizando json-server para simular API & BACK-END
    
    consumindo pelo Fetch

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
+interface Transaction {
+    id: number;
+    description: string;
+    type: 'income' | 'outcome';
+    price: number;
+    category: string;
+    createdAt: string;
+}
+
 export function Transactions(){
+
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+    // Buscando na API
+    async function loadTransactions(){
+        const response = await fetch('http://localhost:3000/transactions');
+        const data = await response.json();
+
+        // Atualizando..
+        setTransactions(data);
+    }
+
+    // Para executar apenas uma unica vez
+    useEffect(() => {
+        loadTransactions();
+    }, []); 
+
     return (
         <div>
             <Header />
@@ -15,26 +42,20 @@ export function Transactions(){
 
                 <TransactionsTable>
                     <tbody>
-                        <tr>
-                            <td width="50%">Desenvolvimento de site</td>
-                            <td>
-                                <PriceHighlight variant="income">
-                                    R$ 12.000,00
-                                </PriceHighlight>
-                            </td>
-                            <td>Venda</td>
-                            <td>13/09/2022</td>
-                        </tr>
-                        <tr>
-                            <td width="50%">Jantar</td>
-                            <td>
-                                <PriceHighlight variant="outcome">
-                                - R$ 29,00
-                                </PriceHighlight>
-                            </td>
-                            <td>Lazer</td>
-                            <td>20/09/2022</td>
-                        </tr>
+                        {transactions.map(transaction => {
+                            return (
+                                <tr key={transaction.id}>
+                                    <td width="50%">{transaction.description}</td>
+                                    <td>
+                                        <PriceHighlight variant={transaction.type}>
+                                            {transaction.price}
+                                        </PriceHighlight>
+                                    </td>
+                                    <td>{transaction.category}</td>
+                                    <td>{transaction.createdAt}</td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
                 </TransactionsTable>
             </TransactionsContainer>
