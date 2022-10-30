42e6919410626a73174ed66cb9b370b1168fd391
João <joaopedronk22@gmail.com>
2022-10-30 15:03:47 -0300

    Aplicando use Callback

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from 'react'
+import { ReactNode, useEffect, useState, useCallback } from 'react'
 import { createContext } from 'use-context-selector'
 import { api } from '../lib/axios'
 
@@ -34,7 +34,7 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([])
 
   // Buscando na API
-  async function fetchTransactions(query?: string) {
+  const fetchTransactions = useCallback(async (query?: string) => {
     const response = await api.get('transactions', {
       params: {
         _sort: 'createdAt',
@@ -45,22 +45,25 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
 
     // Atualizando..
     setTransactions(response.data)
-  }
+  }, [])
 
-  async function createTransaction(data: CreateTransactionInput) {
-    const { description, price, category, type } = data
+  const createTransaction = useCallback(
+    async (data: CreateTransactionInput) => {
+      const { description, price, category, type } = data
 
-    const response = await api.post('transactions', {
-      description,
-      price,
-      category,
-      type,
-      createdAt: new Date(),
-    })
+      const response = await api.post('transactions', {
+        description,
+        price,
+        category,
+        type,
+        createdAt: new Date(),
+      })
 
-    // Atualizando com retorno do post
-    setTransactions((state) => [response.data, ...state])
-  }
+      // Atualizando com retorno do post
+      setTransactions((state) => [response.data, ...state])
+    },
+    [],
+  )
 
   // Para executar apenas uma unica vez
   useEffect(() => {
